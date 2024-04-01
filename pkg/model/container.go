bdcfbb817f78a2ff452f7d04a6991836f7908e16
Đặng Minh Dũng <dungdm93@live.com>
2024-04-01 09:53:16 +0700

    add `Container` model

---
+++ pkg/model/container.go
@@ -0,0 +1,39 @@
+package model
+
+type Container struct {
+	// The Docker image to use as the container to run the action.
+	// The value can be the Docker Hub image name or a registry name.
+	// https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idcontainerimage
+	Image Evaluable[string] `json:"image,omitempty" yaml:"image,omitempty" validate:"required"`
+
+	// If the image's container registry requires authentication to pull the image,
+	// you can use credentials to set a map of the username and password.
+	// The credentials are the same values that you would provide to the `docker login` command.
+	// https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idcontainercredentials
+	Credentials *ContainerCredentials `json:"credentials,omitempty" yaml:"credentials,omitempty"`
+
+	// Sets an array of environment variables in the container.
+	// https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idcontainerenv
+	Env Env `json:"env,omitempty" yaml:"env,omitempty"`
+
+	// Sets an array of ports to expose on the container.
+	// https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idcontainerports
+	Ports []string `json:"ports,omitempty" yaml:"ports,omitempty"`
+
+	// Sets an array of volumes for the container to use. You can use volumes to share data between services or other steps in a job.
+	// You can specify named Docker volumes, anonymous Docker volumes, or bind mounts on the host.
+	// To specify a volume, you specify the source and destination path: <source>:<destinationPath>
+	// The <source> is a volume name or an absolute path on the host machine, and <destinationPath> is an absolute path in the container.
+	// https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idcontainervolumes
+	Volumes []string `json:"volumes,omitempty" yaml:"volumes,omitempty"`
+
+	// Additional Docker container resource options.
+	// For a list of options, see https://docs.docker.com/engine/reference/commandline/create/#options.
+	// https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idcontaineroptions
+	Options string `json:"options,omitempty" yaml:"options,omitempty"`
+}
+
+type ContainerCredentials struct {
+	Username Evaluable[string] `json:"username,omitempty" yaml:"username,omitempty"`
+	Password Evaluable[string] `json:"password,omitempty" yaml:"password,omitempty"`
+}
