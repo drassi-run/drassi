6ac2387282d0ffe13effc22e50059637111d1d7f
Đặng Minh Dũng <dungdm93@live.com>
2024-04-12 09:36:12 +0700

    executor: Job & RunContext first look

---
+++ pkg/executor/job.go
@@ -0,0 +1,43 @@
+package executor
+
+import (
+	"context"
+
+	"github.com/dungdm93/drasi/pkg/model/workflows"
+)
+
+type Stage string
+
+const (
+	StagePre  Stage = "pre"
+	StageMain Stage = "main"
+	StagePost Stage = "post"
+)
+
+type Task struct {
+	StepID    string
+	Stage     Stage
+	Condition workflows.Conditional // default true
+	Run       func(context.Context) error
+}
+
+type JobRunner struct {
+	rCtx   *RunContext
+	job    workflows.Job
+	runner StepsRunner
+}
+
+func (s *JobRunner) Initialize(ctx context.Context) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *JobRunner) Run(ctx context.Context) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s *JobRunner) Finalize(ctx context.Context) error {
+	//TODO implement me
+	panic("implement me")
+}
