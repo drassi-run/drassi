a2e1d113262a8e11abc0e69a0a6cb17ea2afbb49
Đặng Minh Dũng <dungdm93@live.com>
2024-05-06 22:15:29 +0700

    add new `RunnerSession` model

--- pkg/service/gha/types.go
+++ pkg/service/gha/types.go
@@ -131,6 +131,34 @@ func (pk *RunnerPublicKey) ToRsaPublicKey() (*rsa.PublicKey, error) {
 	return &pubkey, nil
 }
 
+// https://github.com/actions/runner/blob/v2.315.0/src/Sdk/DTWebApi/WebApi/TaskAgentSession.cs
+type RunnerSession struct {
+	// The unique identifier for this session
+	Id string `json:"sessionId,omitempty"` // UUID
+
+	// The key used to encrypt message traffic for this session
+	EncryptionKey *RunnerSessionKey `json:"encryptionKey,omitempty"`
+
+	// The owner name of this session. Generally this will be the machine of origination
+	OwnerName string `json:"ownerName,omitempty"`
+
+	// The runner (agent) which is the target of the session
+	Runner *RunnerReference `json:"agent,omitempty"`
+
+	// whether to use FIPS compliant encryption scheme for job message key
+	UseFipsEncryption bool `json:"useFipsEncryption,omitempty"`
+}
+
+// https://github.com/actions/runner/blob/v2.315.0/src/Sdk/DTWebApi/WebApi/TaskAgentSessionKey.cs
+type RunnerSessionKey struct {
+	// The value indicating whether the key value is encrypted. If this value is true, the Value property
+	// should be decrypted using the RSA key exchanged with the server during registration.
+	Encrypted bool `json:"encrypted,omitempty"`
+
+	// The symmetric key value.
+	Value []byte `json:"value,omitempty"`
+}
+
 type Message struct {
 	// The message identifier
 	Id string `json:"messageId,omitempty"`
