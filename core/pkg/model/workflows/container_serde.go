b0611599a55d07ba102a2673aa64ca49b7dc2ab8
Đặng Minh Dũng <dungdm93@live.com>
2024-05-23 10:24:42 +0700

    core/model: refactor container

--- core/pkg/model/workflows/container_serde.go
+++ core/pkg/model/workflows/container_serde.go
@@ -2,8 +2,8 @@ package workflows
 
 func (c *Container) DecodeMapstructure(input any) (any, error) {
 	if image, ok := input.(string); ok {
-		c.Image = image
-		return nil, nil
+		m := map[string]any{"image": image}
+		return m, nil
 	}
 	// process Container normal way
 	return input, nil
