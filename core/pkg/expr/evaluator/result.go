bad61306cd814ea00ecdb9002b87ee9153f740ff
phuongnd96 <nguyenduyphuong_t59@hus.edu.vn>
2024-05-23 14:00:11 +0700

    x

--- core/pkg/expr/evaluator/result.go
+++ core/pkg/expr/evaluator/result.go
@@ -407,6 +407,7 @@ func (e *result) string() string {
 		switch e.value.(type) {
 		case float64:
 			f := e.value.(float64)
+			// chooses between fixed-point and exponential notation based on the value's magnitude and precision, aiming to present the number in the most compact form without losing significant digits.
 			return fmt.Sprintf("%g", f)
 		case int:
 			i := e.value.(int)
