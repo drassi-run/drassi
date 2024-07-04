86052e820b9e4ba8fe18e23aa3932d013d7a2982
Đặng Minh Dũng <dungdm93@live.com>
2024-07-04 22:31:19 +0700

    core/executor: update `DockerStepRun`

--- core/pkg/executor/step_docker.go
+++ core/pkg/executor/step_docker.go
@@ -2,17 +2,37 @@ package executor
 
 import (
 	"context"
+	"fmt"
+	"maps"
+	"strings"
 
 	"drassi.run/core/pkg/executor/evaluator"
 	"drassi.run/core/pkg/model/dossiers"
+	"drassi.run/core/pkg/model/workflows"
 )
 
-// Example:
-// + `uses: docker://alpine:3.8`
-// + `uses: docker://gcr.io/cloud-builders/gradle`
 type DockerStepRun struct {
 	BaseStepRun
+
+	// Should be either:
+	// * docker://image[:tag]
+	// * [/path/to/]Dockerfile
 	Image string
+
+	// Entrypoint for main stage. if empty, with.entrypoint could be use
+	// https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idstepswithentrypoint
+	Entrypoint string
+	// Entrypoint for all stages. if empty, with.args could be use
+	// https://docs.github.com/en/actions/using-workflows/workflow-syntax-for-github-actions#jobsjob_idstepswithargs
+	Args workflows.Evaluable[[]string]
+
+	PreEntrypoint string
+	PreIf         workflows.Conditional
+
+	PostEntrypoint string
+	PostIf         workflows.Conditional
+
+	resolvedImage string
 }
 
 func (sr *DockerStepRun) SetContextInfo(dossier *dossiers.Dossier) {
@@ -24,11 +44,24 @@ func (sr *DockerStepRun) SetContextInfo(dossier *dossiers.Dossier) {
 }
 
 func (sr *DockerStepRun) Initialize(ctx context.Context, exec StepExecutor) error {
-	return exec.Sandbox().PullImage(ctx, sr.Image)
+	if image, ok := strings.CutPrefix(sr.Image, "docker://"); ok {
+		sr.resolvedImage = image
+		return exec.Sandbox().PullImage(ctx, image)
+	}
+	// TODO: build image based on Dockerfile
+	return nil
 }
 
 func (sr *DockerStepRun) PreTask() *Task {
-	return nil
+	if sr.PreEntrypoint == "" {
+		return nil
+	}
+	return &Task{
+		StepId:    sr.Id,
+		Stage:     StagePre,
+		Condition: sr.PreIf,
+		Run:       sr.execute(StagePre),
+	}
 }
 
 func (sr *DockerStepRun) MainTask() *Task {
@@ -36,29 +69,87 @@ func (sr *DockerStepRun) MainTask() *Task {
 		StepId:    sr.Id,
 		Stage:     StageMain,
 		Condition: sr.Condition,
-		Run:       sr.executeMain,
+		Run:       sr.execute(StageMain),
 	}
-}
-
-func (sr *DockerStepRun) executeMain(ctx context.Context, exec StepExecutor) error {
-	inputs := make(map[string]string)
-	if err := evaluator.Evaluate(exec.ExpressionEnv(), sr.Inputs, &inputs); err != nil {
-		return err
-	}
-
-	var entrypoint []string
-	if ep, ok := inputs["entrypoint"]; ok {
-		entrypoint = append(entrypoint, ep)
-	}
-
-	var cmd []string
-	if c, ok := inputs["cmd"]; ok {
-		cmd = append(cmd, c)
-	}
-
-	return exec.Sandbox().RunContainer(ctx, sr.Image, entrypoint, cmd, nil, "")
 }
 
 func (sr *DockerStepRun) PostTask() *Task {
-	return nil
+	if sr.PostEntrypoint == "" {
+		return nil
+	}
+	return &Task{
+		StepId:    sr.Id,
+		Stage:     StagePost,
+		Condition: sr.PostIf,
+		Run:       sr.execute(StagePost),
+	}
+}
+
+// https://github.com/actions/runner/blob/v2.315.0/src/Runner.Worker/Handlers/ContainerActionHandler.cs#L22
+func (sr *DockerStepRun) execute(stage Stage) func(context.Context, StepExecutor) error {
+	return func(ctx context.Context, exec StepExecutor) error {
+		inputs := make(map[string]string)
+		if err := evaluator.Evaluate(exec.ExpressionEnv(), sr.Inputs, &inputs); err != nil {
+			return err
+		}
+
+		entrypoint, err := sr.computeEntrypoint(stage, inputs)
+		if err != nil {
+			return err
+		}
+
+		args, err := sr.computeArgs(inputs, exec)
+		if err != nil {
+			return err
+		}
+
+		env := make(map[string]string)
+		if err := evaluator.Evaluate(exec.ExpressionEnv(), sr.Env, &env); err != nil {
+			return err
+		}
+		maps.Copy(env, exec.ComposeEnv())
+
+		return exec.Sandbox().RunContainer(ctx, sr.resolvedImage, entrypoint, args, env, "")
+	}
+}
+
+func (sr *DockerStepRun) computeEntrypoint(stage Stage, inputs map[string]string) ([]string, error) {
+	var ep string
+	switch stage {
+	case StagePre:
+		ep = sr.PreEntrypoint
+	case StagePost:
+		ep = sr.PostEntrypoint
+	case StageMain:
+		ep = sr.Entrypoint
+	default:
+		return nil, fmt.Errorf("unknown stage: %s", stage)
+	}
+
+	if ep != "" {
+		return []string{ep}, nil
+	}
+
+	if stage == StageMain {
+		if ep, ok := inputs["entrypoint"]; ok {
+			return []string{ep}, nil
+		}
+	}
+	return nil, nil
+}
+
+func (sr *DockerStepRun) computeArgs(inputs map[string]string, exec StepExecutor) ([]string, error) {
+	if sr.Args != nil {
+		args := make([]string, 0)
+		if err := evaluator.Evaluate(exec.ExpressionEnv(), sr.Args, &args); err != nil {
+			return nil, err
+		}
+		return args, nil
+	}
+
+	if args, ok := inputs["args"]; ok {
+		return []string{args}, nil
+	} else {
+		return nil, nil
+	}
 }
