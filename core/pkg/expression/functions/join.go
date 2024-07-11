701f7e640c4eb1a29079f3da2ab3307536e4b85c
Đặng Minh Dũng <dungdm93@live.com>
2024-07-11 23:10:46 +0700

    core/expression: add `Join` functions

---
+++ core/pkg/expression/functions/join.go
@@ -0,0 +1,48 @@
+package functions
+
+import (
+	"strings"
+
+	"drassi.run/core/pkg/expression/types/ref"
+	"drassi.run/core/pkg/expression/types/traits"
+)
+
+func Join(array ref.Val, separator ref.Val) string {
+	if array.Type() == ref.TypeList {
+		if list, ok := array.(traits.Iterable); ok {
+			sep := ","
+			if separator != nil {
+				if s, ok := separator.(traits.Stringable); ok {
+					sep = s.ToString()
+				}
+			}
+
+			res := new(strings.Builder)
+			it := list.Iterator()
+			i := 0
+			for it.HasNext() {
+				_, e := it.Next()
+				i++
+
+				if i > 1 {
+					res.WriteString(sep)
+				}
+				res.WriteString(display(e))
+			}
+
+			return res.String()
+		}
+	}
+
+	if str, ok := array.(traits.Stringable); ok {
+		return str.ToString()
+	}
+	return ""
+}
+
+func display(v ref.Val) string {
+	if s, ok := v.(traits.Stringable); ok {
+		return s.ToString()
+	}
+	return v.Type().String()
+}
