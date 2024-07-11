0cc1822c08ee2b34120b78807568ad4e514e6a29
Đặng Minh Dũng <dungdm93@live.com>
2024-07-11 22:35:31 +0700

    core/expression: add string functions

---
+++ core/pkg/expression/functions/strings.go
@@ -0,0 +1,47 @@
+package functions
+
+import (
+	"strings"
+
+	"drassi.run/core/pkg/expression/types/ref"
+	"drassi.run/core/pkg/expression/types/traits"
+)
+
+func StartsWith(str, prefix ref.Val) bool {
+	s, ok := str.(traits.Stringable)
+	if !ok {
+		return false
+	}
+	f, ok := prefix.(traits.Stringable)
+	if !ok {
+		return false
+	}
+
+	ss := s.ToString()
+	sf := f.ToString()
+	if len(ss) < len(sf) {
+		return false
+	}
+	// GitHub ignores case when comparing strings.
+	return strings.EqualFold(ss[:len(sf)], sf)
+}
+
+func EndsWith(str, suffix ref.Val) bool {
+	s, ok := str.(traits.Stringable)
+	if !ok {
+		return false
+	}
+	f, ok := suffix.(traits.Stringable)
+	if !ok {
+		return false
+	}
+
+	// GitHub ignores case when comparing strings.
+	ss := s.ToString()
+	sf := f.ToString()
+	if len(ss) < len(sf) {
+		return false
+	}
+	// GitHub ignores case when comparing strings.
+	return strings.EqualFold(ss[len(ss)-len(sf):], sf)
+}
