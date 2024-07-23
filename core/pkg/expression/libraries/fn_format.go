93e1a40d34da5899b56eb8b09f9e877da1cae2e2
Đặng Minh Dũng <dungdm93@live.com>
2024-07-23 12:45:27 +0700

    core/expression: implement `Format` function

---
+++ core/pkg/expression/libraries/fn_format.go
@@ -0,0 +1,108 @@
+package libraries
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"unicode"
+
+	"drassi.run/core/pkg/expression/types"
+	"drassi.run/core/pkg/expression/types/ref"
+)
+
+const (
+	passThrough = iota
+	braceOpen
+	braceClose
+	placeHolder
+)
+
+var errInvalidFormat = errors.New("invalid format")
+
+func Format(fmt ref.LazyVal, args ...ref.LazyVal) ref.Val {
+	v := fmt()
+	if v.Type() == ref.TypeInvalid {
+		return v
+	}
+	format := display(v)
+	argsCache := make([]*string, len(args))
+
+	output := new(strings.Builder)
+	output.Grow(len(format)) // best-effort to set output length
+
+	state, idx := passThrough, -1
+	for i, c := range format {
+		//goland:noinspection GoDfaConstantCondition
+		switch state {
+		case passThrough: // normal buffer output
+			switch c {
+			case '{':
+				state, idx = braceOpen, i+1
+			case '}':
+				state = braceClose
+			default:
+				output.WriteRune(c)
+			}
+
+		case braceOpen: // found '{'
+			if c == '{' {
+				// unescape "{{"
+				output.WriteRune('{')
+				state, idx = passThrough, -1
+				break
+			}
+
+			state = placeHolder
+			fallthrough
+
+		case placeHolder:
+			if c != '}' {
+				if !unicode.IsDigit(c) {
+					return types.WrapError(errInvalidFormat)
+				}
+				break
+			}
+			// end placeholder, substitute it with replacement value
+			index, err := strconv.Atoi(format[idx:i])
+			if err != nil {
+				return types.NewError("%w: %w", errInvalidFormat, err)
+			}
+			if index < 0 || index >= len(args) {
+				return types.NewError("index out of range: [0..%d), got %d", len(args), index)
+			}
+
+			// compute args & cache result
+			rep := argsCache[index]
+			if rep == nil {
+				v := args[index]()
+				if v.Type() == ref.TypeInvalid {
+					return v
+				}
+				s := display(v)
+				rep = &s
+				argsCache[index] = rep
+			}
+			output.WriteString(*rep)
+			state, idx = passThrough, -1
+
+		case braceClose: // found '}'
+			if c == '}' {
+				// unescape "}}"
+				output.WriteRune('}')
+				state, idx = passThrough, -1
+				break
+			}
+			return types.WrapError(errInvalidFormat)
+		}
+	}
+
+	switch state {
+	case placeHolder, braceOpen:
+		return types.NewError("unclosed brace at %d: %w", idx, errInvalidFormat)
+	case braceClose:
+		return types.NewError("closing brace without opening %d: %w", idx, errInvalidFormat)
+	default:
+		s := output.String()
+		return types.String(s)
+	}
+}
