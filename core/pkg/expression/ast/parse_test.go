9b888883df207b821fd10d03bf6f95a99d97739b
Đặng Minh Dũng <dungdm93@live.com>
2024-08-03 09:23:06 +0700

    core/expression: test `ParseTemplate`

--- core/pkg/expression/ast/parse_test.go
+++ core/pkg/expression/ast/parse_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestParse(t *testing.T) {
+func TestParseExpression(t *testing.T) {
 	t.Run("simple", testParseSimple)
 	t.Run("ops-order", testParseOperatorOrder)
 	t.Run("error", testParseError)
@@ -196,4 +196,112 @@ func testParseOptions(t *testing.T) {
 		assert.Error(t, err)
 		assert.ErrorContains(t, err, "more than 5 errors occurred")
 	})
+}
+
+func TestParseTemplate(t *testing.T) {
+	t.Run("raw-string", testParseTemplateRawString)
+	t.Run("single-expr", testParseTemplateSingleExpr)
+	t.Run("with-expr", testParseTemplateWithExpr)
+}
+
+func testParseTemplateRawString(t *testing.T) {
+	tests := []string{
+		`foobar`,
+		`$`,
+		`${`,
+		`${xxxxx`,
+		`${x${xxx`,
+		`${we}${❤️}${δράση}`,
+		`$x{xxxx`,
+		`$x{{xxx`,
+		`}}`,
+		`}}}}}}`,
+		`'foobar'`,
+		`"foobar"`,
+	}
+	for _, input := range tests {
+		node, err := ParseTemplate(input)
+		expected := &LiteralNode{types.String(input)}
+
+		assert.NoError(t, err)
+		assert.EqualValues(t, expected, node, input)
+	}
+}
+
+func testParseTemplateSingleExpr(t *testing.T) {
+	tests := map[string]Node{
+		`${{ a }}`:         &VariableNode{"a"},
+		`${{ null }}`:      &LiteralNode{types.NULL},
+		`${{ true }}`:      &LiteralNode{types.TRUE},
+		`${{ false }}`:     &LiteralNode{types.FALSE},
+		`${{ Infinity }}`:  &LiteralNode{types.POSITIVE_INF},
+		`${{ -Infinity }}`: &LiteralNode{types.NEGATIVE_INF},
+		`${{ 'foobar' }}`:  &LiteralNode{types.String("foobar")},
+		`${{ '${{' }}`:     &LiteralNode{types.String("${{")},
+	}
+
+	for source, expected := range tests {
+		node, err := ParseTemplate(source)
+
+		assert.NoError(t, err)
+		assert.EqualValues(t, expected, node, source)
+	}
+}
+
+func testParseTemplateWithExpr(t *testing.T) {
+	tests := map[string]Node{
+		`abc${{ a }}xyz`: &FunctionNode{"format", []Node{
+			&LiteralNode{types.String("abc{0}xyz")},
+			&VariableNode{"a"},
+		}},
+		`abc${{ '${{' }}xyz`: &FunctionNode{"format", []Node{
+			&LiteralNode{types.String("abc{0}xyz")},
+			&LiteralNode{types.String("${{")},
+		}},
+		`$${{ '${{' }}`: &FunctionNode{"format", []Node{
+			&LiteralNode{types.String("${0}")},
+			&LiteralNode{types.String("${{")},
+		}},
+		`${${{ '${{' }}`: &FunctionNode{"format", []Node{
+			&LiteralNode{types.String("${{0}")},
+			&LiteralNode{types.String("${{")},
+		}},
+		`${}${{ '${{' }}`: &FunctionNode{"format", []Node{
+			&LiteralNode{types.String("${}{0}")},
+			&LiteralNode{types.String("${{")},
+		}},
+		`${x}${{ '${{' }}`: &FunctionNode{"format", []Node{
+			&LiteralNode{types.String("${x}{0}")},
+			&LiteralNode{types.String("${{")},
+		}},
+		`${{ a }}${{ '${{' }}`: &FunctionNode{"format", []Node{
+			&LiteralNode{types.String("{0}{1}")},
+			&VariableNode{"a"},
+			&LiteralNode{types.String("${{")},
+		}},
+		`${{ a }}${x}${{ '${{' }}`: &FunctionNode{"format", []Node{
+			&LiteralNode{types.String("{0}${x}{1}")},
+			&VariableNode{"a"},
+			&LiteralNode{types.String("${{")},
+		}},
+		`${xx${yy${{ '${{' }}`: &FunctionNode{"format", []Node{
+			&LiteralNode{types.String("${xx${yy{0}")},
+			&LiteralNode{types.String("${{")},
+		}},
+		"${xx\n${yy${{ '${{' }}": &FunctionNode{"format", []Node{
+			&LiteralNode{types.String("${xx\n${yy{0}")},
+			&LiteralNode{types.String("${{")},
+		}},
+		`${we}${{ '❤️' }}${δράση}`: &FunctionNode{"format", []Node{
+			&LiteralNode{types.String("${we}{0}${δράση}")},
+			&LiteralNode{types.String("❤️")},
+		}},
+	}
+
+	for source, expected := range tests {
+		node, err := ParseTemplate(source)
+
+		assert.NoError(t, err)
+		assert.EqualValues(t, expected, node, source)
+	}
 }
