8feb31aff029302f539a8987ab8ae7ed1c7b6633
Đặng Minh Dũng <dungdm93@live.com>
2024-09-22 22:26:57 +0700

    core/model: add missing `actions` tag

--- core/pkg/model/records/job.go
+++ core/pkg/model/records/job.go
@@ -4,22 +4,22 @@ package records
 // https://docs.github.com/en/actions/learn-github-actions/contexts#job-context
 // https://github.com/actions/runner/blob/v2.315.0/src/Runner.Worker/JobContext.cs
 type JobInfo struct {
-	Container *Container            `json:"container" yaml:"container"`
-	Services  map[string]*Container `json:"services" yaml:"services"`
-	Status    Result                `json:"status" yaml:"status"`
+	Container *Container            `json:"container" yaml:"container" actions:"container"`
+	Services  map[string]*Container `json:"services" yaml:"services" actions:"services"`
+	Status    Result                `json:"status" yaml:"status" actions:"status"`
 }
 
 type Container struct {
-	Id      string            `json:"id" yaml:"id"`
-	Network string            `json:"network" yaml:"network"`
-	Ports   map[string]string `json:"ports" yaml:"ports"`
+	Id      string            `json:"id" yaml:"id" actions:"id"`
+	Network string            `json:"network" yaml:"network" actions:"network"`
+	Ports   map[string]string `json:"ports" yaml:"ports" actions:"ports"`
 }
 
 // The `jobs` context is only available in reusable workflows, and can only be used to set outputs for a reusable workflow.
 // https://docs.github.com/en/actions/learn-github-actions/contexts#jobs-context
 type Job struct {
-	Result  Result            `json:"result" yaml:"result"`
-	Outputs map[string]string `json:"outputs" yaml:"outputs"`
+	Result  Result            `json:"result" yaml:"result" actions:"result"`
+	Outputs map[string]string `json:"outputs" yaml:"outputs" actions:"outputs"`
 }
 
 // The `steps` context contains information about the steps in the current job that have an `id` specified and have already run.
--- core/pkg/model/records/runner.go
+++ core/pkg/model/records/runner.go
@@ -5,12 +5,12 @@ import "drassi.run/core/pkg/model"
 // The `runner` context contains information about the runner that is executing the current job.
 // https://docs.github.com/en/actions/learn-github-actions/contexts#runner-context
 type Runner struct {
-	Name        string             `json:"name" yaml:"name"`
-	Os          model.Machine      `json:"os" yaml:"os"`
-	Arch        model.Architecture `json:"arch" yaml:"arch"`
-	Environment string             `json:"environment" yaml:"environment"`
-	Temp        string             `json:"temp" yaml:"temp"`
-	ToolCache   string             `json:"tool_cache" yaml:"tool_cache"`
-	Workspace   string             `json:"workspace" yaml:"workspace"`
-	Debug       string             `json:"debug" yaml:"debug"`
+	Name        string             `json:"name" yaml:"name" actions:"name"`
+	Os          model.Machine      `json:"os" yaml:"os" actions:"os"`
+	Arch        model.Architecture `json:"arch" yaml:"arch" actions:"arch"`
+	Environment string             `json:"environment" yaml:"environment" actions:"environment"`
+	Temp        string             `json:"temp" yaml:"temp" actions:"temp"`
+	ToolCache   string             `json:"tool_cache" yaml:"tool_cache" actions:"tool_cache"`
+	Workspace   string             `json:"workspace" yaml:"workspace" actions:"workspace"`
+	Debug       string             `json:"debug" yaml:"debug" actions:"debug"`
 }
