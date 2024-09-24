7ec2e71a7039a2b9f03ba2c240a5bcd35a95b06a
Đặng Minh Dũng <dungdm93@live.com>
2024-09-24 17:07:40 +0700

    core/store: resolve user home folder `~`

--- core/pkg/store/repository/gitstore/store.go
+++ core/pkg/store/repository/gitstore/store.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"io/fs"
 	"os"
+	"os/user"
 	"path/filepath"
 	"strings"
 
@@ -28,6 +29,12 @@ type Store interface {
 }
 
 func New(rootDir string) (Store, error) {
+	if d, err := resolveDir(rootDir); err != nil {
+		return nil, err
+	} else {
+		rootDir = d
+	}
+
 	if err := os.MkdirAll(rootDir, 0x755); err != nil {
 		return nil, err
 	}
@@ -266,4 +273,26 @@ func url(repo *repository.Repository) string {
 		return "https://" + u
 	}
 	return repo.Transport + "://" + u
+}
+
+func resolveDir(path string) (string, error) {
+	if len(path) > 0 && path[0] == '~' {
+		var u *user.User
+		var err error
+		idx := strings.IndexByte(path, os.PathSeparator)
+		if idx < 0 {
+			idx = len(path)
+		}
+		if username := path[1:idx]; username == "" {
+			u, err = user.Current()
+		} else {
+			u, err = user.Lookup(username)
+		}
+		if err != nil {
+			return "", err
+		}
+		path = filepath.Join(u.HomeDir, path[idx:])
+	}
+
+	return filepath.Abs(path)
 }
