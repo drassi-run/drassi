900c66bc0cae970eaaa51c5d78d4ae8eb51a0073
Đặng Minh Dũng <dungdm93@live.com>
2025-02-26 17:35:25 +0700

    core/executor: add telemetry wrapper for `JobExecutor` and `StepExecutor`

--- core/pkg/executor/telemetry.go
+++ core/pkg/executor/telemetry.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"drassi.run/core/pkg/model/records"
 	"drassi.run/core/util/dig"
 	"drassi.run/core/util/otel"
 	"go.opentelemetry.io/otel/trace"
@@ -75,4 +76,130 @@ func (sr *telemetryStepRun) decorateTask(fn func(StepRun) *Task) *Task {
 		return delegate(ctx, se)
 	}
 	return task
+}
+
+type telemetryStepExecutor struct {
+	StepExecutor
+	supervisor Supervisor
+}
+
+func WithTelemetryStepExecutor(exec StepExecutor) StepExecutor {
+	if _, ok := exec.(*telemetryStepExecutor); ok {
+		return exec
+	}
+	return &telemetryStepExecutor{StepExecutor: exec}
+}
+
+func (e *telemetryStepExecutor) Initialize(ctx context.Context, scope *dig.Scope) (err error) {
+	stepId := StepId(e)
+	ctx, span := xotel.StartSpan(ctx, fmt.Sprintf("StepExecutor.Initialize(%s)", stepId),
+		trace.WithAttributes(xotel.DrassiStep(FullStepId(e))),
+	)
+	defer xotel.EndSpan(span, &err)
+
+	ctx, syslog := xotel.ChildLogger(ctx, "step", stepId)
+	syslog.Infof("initialize step %q", stepId)
+
+	if err = xdig.Populate(scope, &e.supervisor); err != nil {
+		syslog.Errorf("failed to populate supervisor: %v", err)
+		return err
+	}
+
+	stop := e.supervisor.StartContext(ctx)
+	defer stop()
+
+	return e.StepExecutor.Initialize(ctx, scope)
+}
+
+func (e *telemetryStepExecutor) RunStep(ctx context.Context, fn func(StepRun) *Task) *records.Step {
+	task := fn(e.StepRun())
+	if task == nil {
+		return nil
+	}
+
+	stepId := StepId(e)
+	ctx, span := xotel.StartSpan(ctx, fmt.Sprintf("StepExecutor.RunStep(%s)", stepId),
+		trace.WithAttributes(
+			xotel.DrassiStage(string(task.Stage)),
+			xotel.DrassiStep(FullStepId(e)),
+		),
+	)
+	defer span.End()
+
+	ctx, syslog := xotel.ChildLogger(ctx, "step", stepId)
+	syslog.Infof("running step %q", stepId)
+
+	stop := e.supervisor.StartContext(ctx)
+	defer stop()
+
+	res := e.StepExecutor.RunStep(ctx, fn)
+	syslog.Infof("%s step %q completed with outcome=%q conclusion=%q", task.Stage, stepId, res.Outcome, res.Conclusion)
+	return res
+}
+
+type telemetryJobExecutor struct {
+	JobExecutor
+	supervisor Supervisor
+}
+
+func WithTelemetryJobExecutor(exec JobExecutor) JobExecutor {
+	if _, ok := exec.(*telemetryJobExecutor); ok {
+		return exec
+	}
+	return &telemetryJobExecutor{JobExecutor: exec}
+}
+
+func (e *telemetryJobExecutor) Initialize(ctx context.Context, scope *dig.Scope) (err error) {
+	jobId := JobId(e)
+	ctx, span := xotel.StartSpan(ctx, fmt.Sprintf("JobExecutor.Initialize(%s)", jobId),
+		trace.WithAttributes(xotel.DrassiJob(jobId)),
+	)
+	defer xotel.EndSpan(span, &err)
+
+	ctx, syslog := xotel.ChildLogger(ctx, "job", jobId)
+	syslog.Infof("initialize job %q", jobId)
+
+	if err = xdig.Populate(scope, &e.supervisor); err != nil {
+		syslog.Errorf("failed to populate supervisor: %v", err)
+		return err
+	}
+
+	stop := e.supervisor.StartContext(ctx)
+	defer stop()
+
+	return e.JobExecutor.Initialize(ctx, scope)
+}
+
+func (e *telemetryJobExecutor) RunJob(ctx context.Context) *records.Job {
+	jobId := JobId(e)
+	ctx, span := xotel.StartSpan(ctx, fmt.Sprintf("JobExecutor.RunJob(%s)", jobId),
+		trace.WithAttributes(xotel.DrassiJob(jobId)),
+	)
+	defer span.End()
+
+	ctx, syslog := xotel.ChildLogger(ctx, "job", jobId)
+	syslog.Infof("running job %q", jobId)
+
+	stop := e.supervisor.StartContext(ctx)
+	defer stop()
+
+	res := e.JobExecutor.RunJob(ctx)
+	syslog.Infof("job %q completed with result=%q", jobId, res.Result)
+	return res
+}
+
+func (e *telemetryJobExecutor) Finalize(ctx context.Context) (err error) {
+	jobId := JobId(e)
+	ctx, span := xotel.StartSpan(ctx, fmt.Sprintf("JobExecutor.Finalize(%s)", jobId),
+		trace.WithAttributes(xotel.DrassiJob(jobId)),
+	)
+	defer xotel.EndSpan(span, &err)
+
+	ctx, syslog := xotel.ChildLogger(ctx, "job", jobId)
+	syslog.Infof("terminate job %q", jobId)
+
+	stop := e.supervisor.StartContext(ctx)
+	defer stop()
+
+	return e.JobExecutor.Finalize(ctx)
 }
