c868e1f003eed8aebf9caab9cde6c095c6633d6c
Đặng Minh Dũng <dungdm93@live.com>
2025-04-11 15:41:15 +0700

    jobservice

---
+++ runner-gha/pkg/service/run.go
@@ -0,0 +1,130 @@
+package service
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"drassi.run/core/util/http"
+	"drassi.run/gha-runner/pkg/messages"
+	"drassi.run/gha-runner/pkg/types"
+	"github.com/chainguard-dev/clog"
+)
+
+func newClient(url string, hc *http.Client) (*xhttp.Client, error) {
+	client, err := xhttp.NewClient(url)
+	if err != nil {
+		return nil, err
+	}
+
+	client = client.WithDefaultErrorHandler(types.ParseActionsError).
+		WithDefaultHeader("User-Agent", "gha-runner") // TODO
+
+	if hc != nil {
+		client = client.WithHttpClient(hc)
+	}
+	return client, nil
+}
+
+func NewRunService(url string, hc *http.Client) (*RunService, error) {
+	client, err := newClient(url, hc)
+	if err != nil {
+		return nil, err
+	}
+
+	s := &RunService{
+		client: client,
+	}
+	return s, nil
+}
+
+type RunService struct {
+	client *xhttp.Client
+}
+
+func (s *RunService) AcquireJob(ctx context.Context, msgId, billingOwner string) (*messages.PipelineAgentJobRequest, error) {
+	o := acquireJobRequest{
+		JobMessageId:   msgId,
+		RunnerOS:       "Linux",
+		BillingOwnerId: billingOwner,
+	}
+	msg := new(messages.PipelineAgentJobRequest)
+	hr := s.client.Post("acquirejob").
+		WithBodyProvider(xhttp.JsonEncode(&o)).
+		OnSuccess(xhttp.JsonDecode(msg))
+
+	if err := hr.Do(ctx); err != nil {
+		return nil, err
+	}
+
+	return msg, nil
+}
+
+func (s *RunService) RenewJob(ctx context.Context, msg *messages.PipelineAgentJobRequest) {
+	l := clog.FromContext(ctx)
+	req := &renewJobRequest{
+		PlanID: msg.Plan.PlanId,
+		JobId:  msg.JobId,
+	}
+	resp := new(renewJobResponse)
+	hr := s.client.Post("renewjob").
+		WithBodyProvider(xhttp.JsonEncode(req)).
+		OnSuccess(xhttp.JsonDecode(resp))
+	timer := time.NewTimer(0)
+	defer timer.Stop()
+
+	doRenew := func() {
+		if err := hr.Do(ctx); err != nil {
+			l.ErrorContextf(ctx, "renewjob failed: %v", err)
+			return
+		}
+		l.DebugContextf(ctx, "successfully renew job %s, job is valid till %s", msg.JobId, resp.LockedUntil)
+		if d := s.renewAt(resp.LockedUntil); d >= 0 {
+			timer.Reset(d)
+		}
+	}
+
+	for {
+		select {
+		case <-timer.C:
+			doRenew()
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func (s *RunService) renewAt(t time.Time) time.Duration {
+	d := time.Until(t)
+	if d <= 0 {
+		return 0
+	}
+
+	// Renew when 3/4 time pass or 1 minute before expire, whichever later
+	d1 := d * 3 / 4
+	d2 := d - time.Minute
+	if d1 < d2 {
+		return d2
+	} else {
+		return d1
+	}
+}
+
+func (s *RunService) CompleteJob(ctx context.Context, msg *messages.PipelineAgentJobRequest) error {
+	req := &completeJobRequest{
+		PlanID: msg.Plan.PlanId,
+		JobID:  msg.JobId,
+		// TODO
+		//Conclusion:     conclusion,
+		//Outputs:        outputs,
+		//StepResults:    stepResults,
+		//Annotations:    jobAnnotations,
+		//EnvironmentUrl: environmentUrl,
+		//Telemetry:      telemetry,
+		//BillingOwnerId: billingOwnerId,
+	}
+	hr := s.client.Post("completejob").
+		WithBodyProvider(xhttp.JsonEncode(req))
+
+	return hr.Do(ctx)
+}
---
+++ runner-gha/pkg/service/types.go
@@ -0,0 +1,152 @@
+package service
+
+import "time"
+
+type acquireJobRequest struct {
+	JobMessageId   string `json:"jobMessageId,omitempty"`
+	RunnerOS       string `json:"runnerOS,omitempty"`
+	BillingOwnerId string `json:"billingOwnerId,omitempty"`
+}
+
+type renewJobRequest struct {
+	PlanID string `json:"planId,omitempty"`
+	JobId  string `json:"jobId,omitempty"`
+}
+
+type renewJobResponse struct {
+	LockedUntil time.Time `json:"lockedUntil,omitempty"`
+}
+
+type completeJobRequest struct {
+	PlanID         string              `json:"planId,omitempty"`
+	JobID          string              `json:"jobId,omitempty"`
+	Conclusion     string              `json:"conclusion,omitempty"`
+	Outputs        map[string]Variable `json:"outputs,omitempty"`
+	StepResults    []StepResult        `json:"stepResults,omitempty"`
+	Annotations    []Annotation        `json:"annotations,omitempty"`
+	Telemetry      []Telemetry         `json:"telemetry,omitempty"`
+	EnvironmentUrl string              `json:"environmentUrl,omitempty"`
+	BillingOwnerId string              `json:"billingOwnerId,omitempty"`
+}
+
+type Variable struct {
+	Value    string `json:"value,omitempty"`
+	IsSecret bool   `json:"isSecret,omitempty"`
+}
+
+// https://github.com/actions/runner/blob/v2.323.0/src/Sdk/RSWebApi/Contracts/StepResult.cs
+type StepResult struct {
+	Uid               string              `json:"external_id,omitempty"`
+	Number            int                 `json:"number,omitempty"`
+	Name              string              `json:"name,omitempty"`        // e.g "Run actions/checkout@v3"
+	ActionName        string              `json:"action_name,omitempty"` // e.g "actions/checkout"
+	Ref               string              `json:"ref,omitempty"`
+	Type              string              `json:"type,omitempty"`
+	Status            TimelineRecordState `json:"status,omitempty"`
+	Conclusion        TaskResult          `json:"conclusion,omitempty"`
+	StartedAt         time.Time           `json:"started_at,omitempty"`
+	CompletedAt       time.Time           `json:"completed_at,omitempty"`
+	CompletedLogURL   string              `json:"completed_log_url,omitempty"`
+	CompletedLogLines int64               `json:"completed_log_lines,omitempty"`
+	Annotations       []Annotation        `json:"annotations,omitempty"`
+}
+
+// https://github.com/actions/runner/blob/v2.323.0/src/Sdk/RSWebApi/Contracts/Annotation.cs
+type Annotation struct {
+	Level                 AnnotationLevel `json:"level,omitempty"`
+	Message               string          `json:"message,omitempty"`
+	Title                 string          `json:"title,omitempty"`
+	RawDetails            string          `json:"rawDetails,omitempty"`
+	Path                  string          `json:"path,omitempty"`
+	IsInfrastructureIssue bool            `json:"isInfrastructureIssue,omitempty"`
+	StartLine             int64           `json:"startLine,omitempty"`
+	EndLine               int64           `json:"endLine,omitempty"`
+	StartColumn           int64           `json:"startColumn,omitempty"`
+	EndColumn             int64           `json:"endColumn,omitempty"`
+	StepNumber            int64           `json:"stepNumber,omitempty"`
+}
+
+// https://github.com/actions/runner/blob/v2.323.0/src/Sdk/RSWebApi/Contracts/AnnotationLevel.cs
+type AnnotationLevel int
+
+const (
+	AnnotationLevelUnknown AnnotationLevel = iota
+	AnnotationLevelNotice
+	AnnotationLevelWarning
+	AnnotationLevelFailure
+)
+
+func (l AnnotationLevel) String() string {
+	switch l {
+	case AnnotationLevelNotice:
+		return "notice"
+	case AnnotationLevelWarning:
+		return "warning"
+	case AnnotationLevelFailure:
+		return "failure"
+	default:
+		return "unknown"
+	}
+}
+
+// https://github.com/actions/runner/blob/v2.323.0/src/Sdk/RSWebApi/Contracts/Telemetry.cs
+type Telemetry struct {
+	Message string `json:"message,omitempty"`
+	Type    string `json:"type,omitempty"`
+}
+
+// https://github.com/actions/runner/blob/v2.323.0/src/Sdk/DTWebApi/WebApi/TimelineRecordState.cs
+type TimelineRecordState int
+
+const (
+	TimelineRecordStatePending TimelineRecordState = iota
+	TimelineRecordStateInProgress
+	TimelineRecordStateCompleted
+	TimelineRecordStateDelayed
+)
+
+func (s TimelineRecordState) String() string {
+	switch s {
+	case TimelineRecordStatePending:
+		return "pending"
+	case TimelineRecordStateInProgress:
+		return "in_progress"
+	case TimelineRecordStateCompleted:
+		return "completed"
+	case TimelineRecordStateDelayed:
+		return "delayed"
+	default:
+		return "unknown"
+	}
+}
+
+// https://github.com/actions/runner/blob/v2.323.0/src/Sdk/DTWebApi/WebApi/TaskResult.cs
+type TaskResult int
+
+const (
+	TaskResultSucceeded TaskResult = iota
+	TaskResultSucceededWithIssues
+	TaskResultFailed
+	TaskResultCanceled
+	TaskResultSkipped
+	TaskResultAbandoned
+)
+
+func (r TaskResult) String() string {
+	switch r {
+	case TaskResultSucceeded:
+		return "succeeded"
+	case TaskResultSucceededWithIssues:
+		return "succeeded (w/ issues)"
+	case TaskResultFailed:
+		return "failed"
+	case TaskResultCanceled:
+		return "canceled"
+	case TaskResultSkipped:
+		return "skipped"
+	case TaskResultAbandoned:
+		return "abandoned"
+	default:
+		return "unknown"
+	}
+}
