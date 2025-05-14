3b87f9e92794492cb879d7da1d6a9106469848fc
Đặng Minh Dũng <dungdm93@live.com>
2025-05-14 21:01:13 +0700

    refactor holder

--- runner-gha/pkg/holder/types.go
+++ runner-gha/pkg/holder/types.go
@@ -2,6 +2,8 @@ package holder
 
 import "time"
 
+////////////// RunService: request & response //////////////
+
 type acquireJobRequest struct {
 	JobMessageId   string `json:"jobMessageId,omitempty"`
 	RunnerOS       string `json:"runnerOS,omitempty"`
@@ -27,6 +29,33 @@ type completeJobRequest struct {
 	Telemetry      []Telemetry         `json:"telemetry,omitempty"`
 	EnvironmentUrl string              `json:"environmentUrl,omitempty"`
 	BillingOwnerId string              `json:"billingOwnerId,omitempty"`
+}
+
+////////////// RunnerService: request //////////////
+
+// https://github.com/actions/runner/blob/v2.323.0/src/Sdk/DTWebApi/WebApi/TaskAgentJobRequest.cs
+type runnerJobRequest struct {
+	RequestId              int64         `json:"request_id,omitempty"`
+	QueueTime              time.Time     `json:"queue_time,omitempty"`
+	AssignTime             time.Time     `json:"assign_time,omitempty"`
+	ReceiveTime            time.Time     `json:"receive_time,omitempty"`
+	FinishTime             time.Time     `json:"finish_time,omitempty"`
+	Result                 TaskResult    `json:"result,omitempty"`
+	LockedUntil            time.Time     `json:"locked_until,omitempty"`
+	LockToken              string        `json:"lock_token,omitempty"`    // UUID
+	ServiceOwner           string        `json:"service_owner,omitempty"` // UUID
+	HostId                 string        `json:"host_id,omitempty"`       // UUID
+	ScopeId                string        `json:"scope_id,omitempty"`      // UUID
+	PlanType               string        `json:"plan_type,omitempty"`
+	PlanId                 string        `json:"plan_id,omitempty"` // UUID
+	PlanGroup              string        `json:"plan_group,omitempty"`
+	QueueId                int           `json:"queue_id,omitempty"`
+	PoolId                 int           `json:"pool_id,omitempty"`
+	JobId                  string        `json:"job_id,omitempty"` // UUID
+	JobName                string        `json:"job_name,omitempty"`
+	ExpectedDuration       time.Duration `json:"expected_duration,omitempty"`
+	OrchestrationId        string        `json:"orchestration_id,omitempty"`
+	MatchesAllAgentsInPool bool          `json:"matches_all_agents_in_pool,omitempty"`
 }
 
 type Variable struct {
