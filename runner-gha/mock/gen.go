e8d808f3a5c6589b5369453f962cdacd624b0d4e
Đặng Minh Dũng <dungdm93@live.com>
2025-12-16 11:47:02 +0700

    gha/report: gen mock `Conveyor` and `Uploader`

--- runner-gha/mock/gen.go
+++ runner-gha/mock/gen.go
@@ -2,4 +2,8 @@
 
 //go:generate mockgen -source=../pkg/log/chunker.go -destination=log/chunker.go -typed
 //go:generate mockgen -source=../pkg/log/types.go -destination=log/types.go -typed
+//go:generate mockgen -source=../pkg/report/types/appender.go -destination=report/types/appender.go -typed
+//go:generate mockgen -source=../pkg/report/types/conveyor.go -destination=report/types/conveyor.go -typed
+//go:generate mockgen -source=../pkg/report/types/uploader.go -destination=report/types/uploader.go -typed
+//go:generate mockgen -source=../pkg/report/types/types.go -destination=report/types/types.go -typed
 package mock
