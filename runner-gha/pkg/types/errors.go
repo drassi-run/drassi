6f8746e150a3de2fcc1da9691757d9484363975c
Đặng Minh Dũng <dungdm93@live.com>
2026-02-24 22:24:20 +0700

    chores: improve `ParseActionsError`

--- runner-gha/pkg/types/errors.go
+++ runner-gha/pkg/types/errors.go
@@ -35,18 +35,22 @@ func ParseActionsError(code int, header http.Header, body io.Reader) error {
 	}
 
 	data = bytes.TrimPrefix(data, Utf8BOM)
-	contentType, ok := header["Content-Type"]
-	if ok && len(contentType) > 0 && strings.Contains(contentType[0], "text/plain") {
-		actionsError.Err = errors.New(string(data))
+	contentType := header.Get("Content-Type")
+	if contentType == "" {
 		return &actionsError
 	}
 
-	var exception ActionsException
-	if err = json.Unmarshal(data, &exception); err != nil {
-		actionsError.Err = fmt.Errorf("error unmarshalling actions exception: %w", err)
-	} else {
-		actionsError.Err = &exception
+	if strings.HasPrefix(contentType, "text/plain") {
+		actionsError.Err = errors.New(string(data))
+	} else if strings.HasPrefix(contentType, "application/json") {
+		var exception ActionsException
+		if err = json.Unmarshal(data, &exception); err != nil {
+			actionsError.Err = fmt.Errorf("error unmarshalling actions exception: %w", err)
+		} else {
+			actionsError.Err = &exception
+		}
 	}
+
 	return &actionsError
 }
 
