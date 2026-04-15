0fcdf0067d4b0ba40d79d4843a0c394fd1bd8acc
Đặng Minh Dũng <dungdm93@live.com>
2026-04-15 20:04:09 +0700

    chores: rearrange `Streams`

--- core/pkg/stream/streams.go
+++ core/pkg/stream/streams.go
@@ -14,25 +14,15 @@ type Streams interface {
 	Err() io.Writer
 }
 
-type StreamsOption func(*streams)
-
 type streams struct {
 	in  io.Reader
 	out io.Writer
 	err io.Writer
 }
 
-func (s *streams) In() io.Reader {
-	return s.in
-}
-
-func (s *streams) Out() io.Writer {
-	return s.out
-}
-
-func (s *streams) Err() io.Writer {
-	return s.err
-}
+func (s *streams) In() io.Reader  { return s.in }
+func (s *streams) Out() io.Writer { return s.out }
+func (s *streams) Err() io.Writer { return s.err }
 
 func NewStreams(opts ...StreamsOption) Streams {
 	s := new(streams)
@@ -42,20 +32,16 @@ func NewStreams(opts ...StreamsOption) Streams {
 	return s
 }
 
+type StreamsOption func(*streams)
+
 func WithStdin(in io.Reader) StreamsOption {
-	return func(s *streams) {
-		s.in = in
-	}
+	return func(s *streams) { s.in = in }
 }
 
 func WithStdout(out io.Writer) StreamsOption {
-	return func(s *streams) {
-		s.out = out
-	}
+	return func(s *streams) { s.out = out }
 }
 
 func WithStderr(err io.Writer) StreamsOption {
-	return func(s *streams) {
-		s.err = err
-	}
+	return func(s *streams) { s.err = err }
 }
