bdfacf86cac9df043c6a7128b088efdffaefec84
Đặng Minh Dũng <dungdm93@live.com>
2026-06-19 09:36:31 +0700

    gha/wire: `core` module

---
+++ runner-gha/wire/core/collect.go
@@ -0,0 +1,65 @@
+/*
+ * SPDX-FileCopyrightText: (c) 2024 The Drassi Authors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package wire_core
+
+import (
+	"fmt"
+
+	"drassi.run/core/pkg/executor"
+	"go.uber.org/dig"
+)
+
+func collectDecorators(scope *dig.Scope) error {
+	if err := scope.Provide(collectJobRunDecorators); err != nil {
+		return fmt.Errorf("collect JobRunDecorator: %w", err)
+	}
+	if err := scope.Provide(collectStepRunDecorators); err != nil {
+		return fmt.Errorf("collect StepRunDecorator: %w", err)
+	}
+	if err := scope.Provide(collectActionRunDecorators); err != nil {
+		return fmt.Errorf("collect ActionRunDecorator: %w", err)
+	}
+	return nil
+}
+
+type jobRunDecoratorParam struct {
+	dig.In
+
+	Timeline executor.JobRunDecorator `name:"timeline"`
+	Log      executor.JobRunDecorator `name:"log"`
+}
+
+func collectJobRunDecorators(p jobRunDecoratorParam) executor.JobRunDecorator {
+	return executor.MultiJobRunDecorator{
+		p.Timeline,
+		p.Log,
+	}
+}
+
+type stepRunDecoratorParam struct {
+	dig.In
+
+	Timeline executor.StepRunDecorator `name:"timeline"`
+	Log      executor.StepRunDecorator `name:"log"`
+}
+
+func collectStepRunDecorators(p stepRunDecoratorParam) executor.StepRunDecorator {
+	return executor.MultiStepRunDecorator{
+		p.Timeline,
+		p.Log,
+	}
+}
+
+type actionRunDecoratorParam struct {
+	dig.In
+
+	ConsoleCommand executor.ActionRunDecorator `name:"command"`
+}
+
+func collectActionRunDecorators(p actionRunDecoratorParam) executor.ActionRunDecorator {
+	return p.ConsoleCommand
+}
---
+++ runner-gha/wire/core/hooks.go
@@ -0,0 +1,49 @@
+/*
+ * SPDX-FileCopyrightText: (c) 2024 The Drassi Authors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package wire_core
+
+import (
+	"context"
+	"encoding/json/v2"
+	"fmt"
+
+	"drassi.run/core/pkg/executor"
+	"drassi.run/core/pkg/model/records"
+	"drassi.run/core/pkg/scribe"
+	"drassi.run/gha-runner/pkg/messages"
+)
+
+func printSecretSource[R any](gh *records.Github) executor.Hook[R] {
+	return executor.HookFunc[R](func(ctx context.Context, r R) error {
+		scribe.Writef(ctx, "Secret source: %s", gh.SecretSource)
+		return nil
+	})
+}
+
+func printTokenPermissions[R any](req *messages.PipelineAgentJobRequest) executor.Hook[R] {
+	sysVar := req.Variables
+
+	return executor.HookFunc[R](func(ctx context.Context, r R) error {
+		var perm map[string]string
+		if v, ok := sysVar["system.github.token.permissions"]; !ok {
+			return nil
+		} else if err := json.Unmarshal([]byte(v.Value), &perm); err != nil {
+			return fmt.Errorf("unmarshal permissions: %w", err)
+		}
+
+		if len(perm) > 0 {
+			s := scribe.FromContext(ctx)
+			end := s.Groupf("Token Permissions")
+			defer end()
+
+			for k, v := range perm {
+				s.Writef("%s: %s", k, v)
+			}
+		}
+		return nil
+	})
+}
