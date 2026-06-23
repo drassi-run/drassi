2e0449f6202f71939d8932bd30309626ef9fdcb1
Đặng Minh Dũng <dungdm93@live.com>
2026-06-23 15:49:10 +0700

    gitea: refactor worker

--- runner-gitea/pkg/worker/convert.go
+++ runner-gitea/pkg/worker/convert.go
@@ -12,10 +12,8 @@ import (
 	"fmt"
 	"io"
 
-	runnerv1 "code.gitea.io/actions-proto-go/runner/v1"
 	"drassi.run/core/pkg/executor"
 	"drassi.run/core/pkg/model"
-	"drassi.run/core/pkg/model/records"
 	"drassi.run/core/pkg/model/workflows"
 	"gopkg.in/yaml.v3"
 )
@@ -42,27 +40,4 @@ func convertJobSpec(wf *workflows.Workflow) (*executor.JobSpec, error) {
 		return nil, fmt.Errorf("unsupported job type %T", job)
 	}
 	return nil, fmt.Errorf("empty job")
-}
-
-func convertJobNeeds(taskNeeds map[string]*runnerv1.TaskNeed) map[string]*records.Need {
-	if len(taskNeeds) == 0 {
-		return nil
-	}
-
-	needs := make(map[string]*records.Need, len(taskNeeds))
-	for k, n := range taskNeeds {
-		needs[k] = &records.Need{
-			Outputs: n.Outputs,
-			Result:  resultMap[n.Result],
-		}
-	}
-	return needs
-}
-
-var resultMap = map[runnerv1.Result]records.Result{
-	runnerv1.Result_RESULT_UNSPECIFIED: "",
-	runnerv1.Result_RESULT_SUCCESS:     records.ResultSuccess,
-	runnerv1.Result_RESULT_FAILURE:     records.ResultFailure,
-	runnerv1.Result_RESULT_CANCELLED:   records.ResultCancelled,
-	runnerv1.Result_RESULT_SKIPPED:     records.ResultSkipped,
 }
