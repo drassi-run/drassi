c094361b2cdb561c4e7fa6011a0b33bafc3bdbb6
Đặng Minh Dũng <dungdm93@live.com>
2026-06-25 09:09:28 +0700

    mock: gen io.Reader|Writer mock

--- core/mock/gen.go
+++ core/mock/gen.go
@@ -1,15 +1,16 @@
 //go:generate -command mockgen go tool mockgen
 
-//go:generate mockgen -source=../pkg/container/engine.go -destination=container/engine.go -typed
-//go:generate mockgen -source=../pkg/sandboxer/engine.go -destination=sandboxer/engine.go -typed
-//go:generate mockgen -source=../pkg/sandboxer/sandbox.go -destination=sandboxer/sandbox.go -typed
-//go:generate mockgen -source=../pkg/command/console.go -destination=command/console.go -typed
-//go:generate mockgen -source=../pkg/command/file.go -destination=command/file.go -typed
-//go:generate mockgen -source=../pkg/command/cmdtypes/types.go -destination=command/cmdtypes/types.go -typed
-//go:generate mockgen -source=../pkg/command/cmdtypes/issue.go -destination=command/cmdtypes/issue.go -typed
-//go:generate mockgen -source=../pkg/problem/matcher.go -destination=problem/matcher.go -typed
-//go:generate mockgen -source=../pkg/secret/masker.go -destination=secret/masker.go -typed
-//go:generate mockgen -source=../pkg/executor/job_executor.go -destination=executor/job_executor.go -typed
-//go:generate mockgen -source=../pkg/executor/step_executor.go -destination=executor/step_executor.go -typed
-//go:generate mockgen -source=../pkg/stream/handler.go -destination=stream/handler.go -typed
+//go:generate mockgen -typed -destination=sdk/io/io.go io Reader,Writer,ReadCloser,WriteCloser
+//go:generate mockgen -typed -destination=container/engine.go -source=../pkg/container/engine.go
+//go:generate mockgen -typed -destination=sandboxer/engine.go -source=../pkg/sandboxer/engine.go
+//go:generate mockgen -typed -destination=sandboxer/sandbox.go -source=../pkg/sandboxer/sandbox.go
+//go:generate mockgen -typed -destination=command/console.go -source=../pkg/command/console.go
+//go:generate mockgen -typed -destination=command/file.go -source=../pkg/command/file.go
+//go:generate mockgen -typed -destination=command/cmdtypes/types.go -source=../pkg/command/cmdtypes/types.go
+//go:generate mockgen -typed -destination=command/cmdtypes/issue.go -source=../pkg/command/cmdtypes/issue.go
+//go:generate mockgen -typed -destination=problem/matcher.go -source=../pkg/problem/matcher.go
+//go:generate mockgen -typed -destination=secret/masker.go -source=../pkg/secret/masker.go
+//go:generate mockgen -typed -destination=executor/job_executor.go -source=../pkg/executor/job_executor.go
+//go:generate mockgen -typed -destination=executor/step_executor.go -source=../pkg/executor/step_executor.go
+//go:generate mockgen -typed -destination=stream/handler.go -source=../pkg/stream/handler.go
 package mock
