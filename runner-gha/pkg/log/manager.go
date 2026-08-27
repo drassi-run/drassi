4b0db61499c0deaaedd15e86cbeb91617b82fa91
Đặng Minh Dũng <dungdm93@live.com>
2026-08-27 14:32:24 +0700

    gha/log: make `log.session` self-contained

--- runner-gha/pkg/log/manager.go
+++ runner-gha/pkg/log/manager.go
@@ -10,7 +10,6 @@ import (
 	"fmt"
 	"os"
 	"sync"
-	"time"
 
 	"go.opentelemetry.io/otel/attribute"
 )
@@ -42,16 +41,6 @@ func NewManager(dir string, maxSize int64) (*Manager, error) {
 	return m, nil
 }
 
-type session struct {
-	uid   string
-	attrs []attribute.KeyValue
-	mu    sync.Mutex
-	f     *os.File
-	idx   int
-	size  int64
-	lines int
-}
-
 // Manager writes log entries to the local filesystem then
 // dispatches Update Event to active subscribers.
 // Rotation is performed automatically when file size thresholds are met.
@@ -74,6 +63,25 @@ func (m *Manager) Subscribe() <-chan *Event {
 	return ch
 }
 
+func (m *Manager) Start(uid string, attrs ...attribute.KeyValue) error {
+	m.mu.Lock()
+	if _, exists := m.sessions[uid]; exists {
+		m.mu.Unlock()
+		return fmt.Errorf("session %s already started", uid)
+	}
+
+	s := newSession(m.dir, uid, attrs...)
+	m.sessions[uid] = s
+	m.mu.Unlock()
+
+	m.notify(&Event{
+		Uid:   uid,
+		Kind:  OnRecordStart,
+		Attrs: attrs,
+	})
+	return nil
+}
+
 func (m *Manager) Handle(uid string, line string) error {
 	m.mu.RLock()
 	s, ok := m.sessions[uid]
@@ -83,114 +91,17 @@ func (m *Manager) Handle(uid string, line string) error {
 		return nil
 	}
 
-	s.mu.Lock()
-	if s.f == nil {
-		path := m.sessionFile(s)
-		f, err := os.Create(path)
-		if err != nil {
-			s.mu.Unlock()
-			return err
-		}
-		s.f, s.size, s.lines = f, 0, 0
-	}
-
-	// write data
-	if err := s.write(line); err != nil {
-		s.mu.Unlock()
-		return err
-	}
-
-	e := &Event{
-		Uid:   s.uid,
-		Kind:  OnRecordLog,
-		Attrs: s.attrs,
-		Update: &Update{
-			File:     m.sessionFile(s),
-			Complete: false,
-			Offset:   s.size,
-			Line:     s.lines,
-		},
-	}
-
-	if s.size >= m.maxSize {
-		e.Complete = true
-		if err := s.rotate(); err != nil {
-			s.mu.Unlock()
-			return err
-		}
-	}
-	s.mu.Unlock()
-
-	m.notify(e)
-	return nil
-}
-
-func (m *Manager) sessionFile(s *session) string {
-	return fmt.Sprintf("%s/%s.%d.log", m.dir, s.uid, s.idx)
-}
-
-// RFC3339Tick is C# DateTime format with precision of 100 nanoseconds (7 digits)
-const RFC3339Tick = "2006-01-02T15:04:05.0000000Z07:00"
-
-func (s *session) write(line string) error {
-	now := time.Now().UTC()
-	b := now.AppendFormat(nil, RFC3339Tick)
-	b = append(b, ' ')
-	b = append(b, line...)
-	if l := len(line); l == 0 || line[l-1] != '\n' {
-		b = append(b, '\n')
-	}
-
-	n, err := s.f.Write(b)
+	update, err := s.Write(line, m.maxSize)
 	if err != nil {
 		return err
 	}
 
-	s.size += int64(n)
-	s.lines++
-	return nil
-}
-
-func (s *session) rotate() error {
-	_ = s.f.Chmod(0400)
-	if err := s.f.Close(); err != nil {
-		return err
-	}
-
-	s.f, s.lines, s.size = nil, 0, 0
-	s.idx++
-	return nil
-}
-
-func (m *Manager) notify(e *Event) {
-	m.mu.RLock()
-	defer m.mu.RUnlock()
-
-	for _, sub := range m.subs {
-		sub <- e
-	}
-}
-
-func (m *Manager) Start(uid string, attrs ...attribute.KeyValue) error {
-	m.mu.Lock()
-	if _, exists := m.sessions[uid]; exists {
-		m.mu.Unlock()
-		return fmt.Errorf("session %s already started", uid)
-	}
-
-	s := &session{
-		uid:   uid,
-		attrs: attrs,
-	}
-	m.sessions[uid] = s
-	m.mu.Unlock()
-
-	e := &Event{
-		Uid:   uid,
-		Kind:  OnRecordStart,
-		Attrs: attrs,
-	}
-	m.notify(e)
+	m.notify(&Event{
+		Uid:    s.uid,
+		Kind:   OnRecordLog,
+		Attrs:  s.attrs,
+		Update: update,
+	})
 	return nil
 }
 
@@ -206,28 +117,14 @@ func (m *Manager) Stop(uid string) error {
 		return nil
 	}
 
-	s.mu.Lock()
-	e := &Event{
-		Uid:   s.uid,
-		Kind:  OnRecordStop,
-		Attrs: s.attrs,
-	}
-
-	var rotErr error
-	if s.f != nil {
-		e.Update = &Update{
-			File:     m.sessionFile(s),
-			Complete: true,
-			Offset:   s.size,
-			Line:     s.lines,
-		}
-
-		rotErr = s.rotate()
-	}
-	s.mu.Unlock()
-
-	m.notify(e)
-	return rotErr
+	update, err := s.Stop()
+	m.notify(&Event{
+		Uid:    s.uid,
+		Kind:   OnRecordStop,
+		Attrs:  s.attrs,
+		Update: update,
+	})
+	return err
 }
 
 func (m *Manager) Close() error {
@@ -237,11 +134,7 @@ func (m *Manager) Close() error {
 	m.mu.Unlock()
 
 	for _, s := range sessions {
-		s.mu.Lock()
-		if s.f != nil {
-			_ = s.rotate()
-		}
-		s.mu.Unlock()
+		_ = s.Close()
 	}
 
 	for _, sub := range subs {
@@ -252,4 +145,13 @@ func (m *Manager) Close() error {
 
 func (m *Manager) Dispose() error {
 	return os.RemoveAll(m.dir)
+}
+
+func (m *Manager) notify(e *Event) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	for _, sub := range m.subs {
+		sub <- e
+	}
 }
