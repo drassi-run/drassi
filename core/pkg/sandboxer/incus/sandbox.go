83e9a18b91ceacacd97a33c0334f2529ec3966ea
Đặng Minh Dũng <dungdm93@live.com>
2026-09-14 19:18:01 +0700

    sandboxer/incus: move factory to a separated file

--- core/pkg/sandboxer/incus/sandbox.go
+++ core/pkg/sandboxer/incus/sandbox.go
@@ -249,3 +249,30 @@ func (sb *sandbox) Dialer(cmd []string) func(ctx context.Context, network, addr 
 		return conn, nil
 	}
 }
+
+func getPath(client incusclient.InstanceServer, inst string) (string, error) {
+	req := incusapi.InstanceExecPost{
+		Command:     []string{"true"},
+		WaitForWS:   true,
+		Interactive: false,
+	}
+	op, err := client.ExecInstance(inst, req, nil)
+	if err != nil {
+		return "", err
+	}
+
+	metadata := op.Get().Metadata
+	if env, ok := metadata["environment"]; ok {
+		if m, ok := env.(map[string]string); ok {
+			return m["PATH"], nil
+		}
+		if m, ok := env.(map[string]any); ok {
+			if path, ok := m["PATH"].(string); ok {
+				return path, nil
+			}
+		}
+	}
+	_ = op.Cancel()
+
+	return "", nil
+}
